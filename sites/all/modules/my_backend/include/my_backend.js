8647d9881dab685df55460e9c9453cf0c6d63d61
jercin <jercin@reasonat>
2016-05-15 14:54:16 +0200

    where did this go to ?

---
+++ sites/all/modules/my_backend/include/my_backend.js
@@ -0,0 +1,10 @@
+ jQuery(document).ready(function() {
+
+    var hamburgergpopupmenu = jQuery('.panels-flexible-row.panels-flexible-row-custom_front_panel-4.clearfix.popup.region').html();
+    jQuery('.modal-body').append(hamburgergpopupmenu);
+});
+//jQuery(window).load(function() {
+//    jQuery('.flexslider').flexslider({
+//            animation: "slide"
+//    });
+//});
