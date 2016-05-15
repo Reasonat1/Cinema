2822c3e14a510d981b22b196e0ddcd0bd012b227
yakoub abaya <yakoub@reasonat.com>
2016-05-15 12:36:49 +0300

    haldner not firing

--- sites/all/modules/cinematic_custom/cinematic_toptix/basket.js
+++ sites/all/modules/cinematic_custom/cinematic_toptix/basket.js
@@ -1,5 +1,8 @@
 var toptix_basket_url = null;
 (function($) {
+  if ($.cookie('toptix_basket') == null) {
+    $.cookie('toptix_basket', 0);
+  }
   Drupal.behaviors.toptix_basket_frame = {
     
     attach: function(context, settings) {
@@ -7,6 +10,11 @@ var toptix_basket_url = null;
       $('.toptix-basket').click(function(event) {
         toptix_basket_url = this.dataset.url;
         $esro.getCustomerDetails('toptix_callback_basket_get_customer');
+      });
+
+      $esro.attachEventHandler('basketChanged', function(basket) {
+        console.log(basket);
+        $.cookie('toptix_basket', basket.totalValue);
       });
     },
   };
@@ -35,18 +43,16 @@ function toptix_callback_basket_login(result) {
 }
 
 function toptix_open_basket_frame() {
-  var frame = jQuery('<iframe></iframe>');
-  frame.attr('src', toptix_basket_url);
-  frame.attr('width', '1024');
-  frame.attr('height', '768');
+  var frame = jQuery('#toptix-frame-wrapper');
+  $esro.gotoUrl(toptix_basket_url);
   frame.dialog({
     'title': Drupal.t('Tickets basket'),
     'width': 1024,
     'height': 768,
     'modal': true,
     'open': function(event, ui) {
-      event.target.style.width = "1024px";
-      event.target.style.height = "768px";
+      //event.target.style.width = "1024px";
+      //event.target.style.height = "768px";
     }
   });
 }
