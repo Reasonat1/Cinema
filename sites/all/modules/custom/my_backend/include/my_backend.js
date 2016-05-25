ad78b7b8d9fcf7d7b18fa71ad16dc89e0590d0ed
pradeep981 <pradeep.sharma@brihaspatitech.com>
2016-05-25 19:51:11 +0530

    Implemening the feature :- complex calender view task in-progress state

--- sites/all/modules/custom/my_backend/include/my_backend.js
+++ sites/all/modules/custom/my_backend/include/my_backend.js
@@ -2,4 +2,10 @@
 
     var hamburgergpopupmenu = jQuery('.panels-flexible-row.panels-flexible-row-custom_front_panel-4.clearfix.popup.region').html();
     jQuery('.modal-body').append(hamburgergpopupmenu);
+    jQuery('.calender-full-row .calender-row.inner').click(function(){
+         jQuery(this).parent().find('.calender-popup').addClass('visible');         
+     });
+    jQuery('.close-calender-popup').click(function(){
+        jQuery(this).parent().removeClass('visible');                                             
+    });
 });
