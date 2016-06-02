a3efff3f789f115cf88afc9571433f46f82465d6
pradeep981 <pradeep.sharma@brihaspatitech.com>
2016-06-02 19:32:39 +0530

    Implementing the feature :- task Theme fixes --- Done

--- sites/all/themes/cinemateque/js/cinemateque.script.js
+++ sites/all/themes/cinemateque/js/cinemateque.script.js
@@ -4,7 +4,14 @@
  */
 (function ($) {
   // Add your code here.
-
+    /**
+   * Hamburger menu popup
+   */
+  $(document).ready(function(){
+      var hamburgergpopupmenu = $('.panels-flexible-row.panels-flexible-row-custom_front_panel-4.clearfix.popup.region').html();
+      $('.modal-body').append(hamburgergpopupmenu);
+  });
+  
 })(jQuery);
   jQuery(window).scroll(function() { 
    if( jQuery(window).scrollTop() >29 && !(jQuery('#header').hasClass('sticky-header')))
