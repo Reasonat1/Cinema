443f4030efd9177871fc5291fe965327ac52b4da
Debasish <debasish.mori@gmail.com>
2016-06-09 23:47:15 +0530

    node

--- sites/all/themes/cinemateque/templates/view/views-view-unformatted--lobby--panel-pane-1.tpl.php
+++ sites/all/themes/cinemateque/templates/view/views-view-unformatted--lobby--panel-pane-1.tpl.php
@@ -24,7 +24,7 @@ $results=$view->result;
 foreach ($results as $val) {
   $nid = $val->node_taxonomy_index_nid;
   $node = node_load($nid);
-  drupal_set_message('<pre>'.print_r($nid, 1).'</pre>');
+  drupal_set_message('<pre>'.print_r($node, 1).'</pre>');
 }
 //$current_nid = $results[0]->nid;
 //    $tagetId = $results[0]->field_field_cm_event_lineup[0]['raw']['target_id'];
