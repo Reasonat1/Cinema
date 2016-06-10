d79413dc1c96122dc6538b24d2a3f07ba1e044e8
pradeep981 <pradeep.sharma@brihaspatitech.com>
2016-06-10 20:07:49 +0530

    Task node teaser pane

--- sites/all/modules/custom/my_pane/my-pane-gallery.js
+++ sites/all/modules/custom/my_pane/my-pane-gallery.js
@@ -2,6 +2,7 @@ Drupal.settings.gallery_settings = Drupal.settings.gallery_settings || {};
 
 (function ($) {
 
+
 $('.custom-slideshow').cycle({
     fx: 'scrollHorz',
     prev:    '#prev2', 
@@ -14,11 +15,19 @@ $('.caruosel-slideshow').cycle({
     next:    '#next', 
   });
   
-$(".caruosel-slideshow").mouseover(function(){
-    $(this).cycle('pause');
-}).mouseout(function(){
-    $(this).cycle('resume');
-});
+ //on hover stop the slideshow
+    $(".caruosel-slideshow").mouseover(function(){
+      $(this).cycle('pause');
+    }).mouseout(function(){
+      $(this).cycle('resume');
+    });
+    
+     //on hover stop the slideshow
+   $(".custom-slideshow").mouseover(function(){
+      $(this).cycle('pause');
+    }).mouseout(function(){
+      $(this).cycle('resume');
+    });
     
   $( document ).ajaxComplete(function() {
         $('.custom-slideshow').cycle({
@@ -31,6 +40,49 @@ $(".caruosel-slideshow").mouseover(function(){
             prev:    '#prev', 
             next:    '#next', 
        });
+       
+       // hide/show field based on content selection
+         if($('select#edit-content-type').val()  != 'cm_event' && $('select#edit-content-type').val()  != 'cm_movie' && $('select#edit-content-type').val()  != 'cm_movie_group' ){
+            $('.form-item-top-black-title').hide();
+            $('.form-item-top-white-title').hide();
+            $('.form-item-movie-image').hide();
+            $('.form-item-movie-image-side').hide();      
+            $('.form-item-movie-title').hide();
+            $('.form-item-movie-metainfo').hide();
+            $('.form-item-upcomming-event').hide();
+            $('.form-item-movie-teaser-text-').hide();
+        }else{
+            $('.form-item-top-black-title').show();
+            $('.form-item-top-white-title').show();
+            $('.form-item-movie-image').show();
+            $('.form-item-movie-image-side').show();      
+            $('.form-item-movie-title').show();
+            $('.form-item-movie-metainfo').show();
+            $('.form-item-upcomming-event').show();
+            $('.form-item-movie-teaser-text-').show();            
+        }
+        
+       $('select#edit-content-type').change(function(){
+        if($(this).val()  != 'cm_event' && $(this).val()  != 'cm_movie' && $(this).val()  != 'cm_movie_group' ){
+            $('.form-item-top-black-title').hide();
+            $('.form-item-top-white-title').hide();
+            $('.form-item-movie-image').hide();
+            $('.form-item-movie-image-side').hide();      
+            $('.form-item-movie-title').hide();
+            $('.form-item-movie-metainfo').hide();
+            $('.form-item-upcomming-event').hide();
+            $('.form-item-movie-teaser-text-').hide();
+        }else{
+            $('.form-item-top-black-title').show();
+            $('.form-item-top-white-title').show();
+            $('.form-item-movie-image').show();
+            $('.form-item-movie-image-side').show();      
+            $('.form-item-movie-title').show();
+            $('.form-item-movie-metainfo').show();
+            $('.form-item-upcomming-event').show();
+            $('.form-item-movie-teaser-text-').show();            
+        }
+        });
   });
   
 })(jQuery);
