71abe85634c89bd0364d55c58446c1de6b69e8f7
Debasish <debasish.mori@gmail.com>
2016-06-16 17:26:36 +0530

    css

--- sites/all/modules/custom/my_utilities/include/my_utilities.js
+++ sites/all/modules/custom/my_utilities/include/my_utilities.js
@@ -21,7 +21,7 @@
            if(jQuery('.top-text-blk-wht .white').is(':empty')){
                jQuery('.top-text-blk-wht').hide();
            }
-           if(!!jQuery("slide-alt-title .grp-title:empty").length == false){
-               //jQuery('.slide-alt-title').hide();
+           if(!jQuery.trim( jQuery(".slide-alt-title .grp-title").html() ) == true){
+               jQuery('.slide-alt-title').hide();
            }
 });
