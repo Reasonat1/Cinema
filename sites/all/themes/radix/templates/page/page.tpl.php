8cc6bcf89f89cb02c58efce9eca23c3deaa3248f
name <avinashmishra.vll@gmail.com>
2016-06-20 18:22:29 +0530

    pushing Hide flag field from all results

--- sites/all/themes/radix/templates/page/page.tpl.php
+++ sites/all/themes/radix/templates/page/page.tpl.php
@@ -158,7 +158,7 @@
 <script>
 
     jQuery(document).ready(function () {
-        /*
+        
 		var popup = jQuery(".popup_search").html();
         jQuery(".popup-element-title span").html(popup);
 //        jQuery(".popup_search").html(popup_search);
@@ -168,7 +168,7 @@
 //        jQuery('.search_new').remove();
 //        jQuery('#search-api-page-search-form').append('<div class="search_new">' + data + '</div>');
         jQuery(".search_new").appendTo("#search-api-page-search-form");
-        jQuery("#search-api-page-search-form").appendTo(".container:first");*/
+        jQuery("#search-api-page-search-form").appendTo(".container:first");
         jQuery('select').select2();
     });
 </script>
