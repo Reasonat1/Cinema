f80649695cf538640f76b70e2d256b122d2969f3
jercin <jercin@reasonat>
2016-06-21 15:48:19 +0200

    new modules introduced

--- sites/all/themes/radix/templates/page/page.tpl.php
+++ sites/all/themes/radix/templates/page/page.tpl.php
@@ -154,25 +154,3 @@
         </div>
     <?php endif; ?>
 </footer>
-
-<<<<<<< HEAD
-=======
-<script>
-
-    jQuery(document).ready(function () {
-        var popup = jQuery(".popup_search").html();
-        
-        jQuery(".popup-element-title span").html(popup);
-//        jQuery(".popup_search").html(popup_search);
-//        jQuery(".popup_search").hide();
-//        alert(jQuery('.search-api-page-search-form').html());
-//        var data = jQuery('.search_new').html();
-//        jQuery('.search_new').remove();
-//        jQuery('#search-api-page-search-form').append('<div class="search_new">' + data + '</div>');
-        jQuery(".search_new").appendTo("#search-api-page-search-form");
-        jQuery("#search-api-page-search-form").appendTo(".container:first");
-        jQuery('select').select2();
-    });
-</script>
->>>>>>> b2cdab96d0f776b2892fd3e12eeccae239332d11
-
