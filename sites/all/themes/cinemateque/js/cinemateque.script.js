2de47fbfd93c10fb6e90191fd53f4a57ddeb4970
pradeep981 <pradeep.sharma@brihaspatitech.com>
2016-06-22 20:17:09 +0530

    hamburger menu updated

--- sites/all/themes/cinemateque/js/cinemateque.script.js
+++ sites/all/themes/cinemateque/js/cinemateque.script.js
@@ -13,10 +13,15 @@
         var html = '<div id="myModal" class="modal fade hamburger" role="dialog"><div class="modal-dialog"><div class="modal-content"> <img src="sites/all/themes/cinemateque/images/close42.png" class="close" data-dismiss="modal"/><div class="modal-body"></div></div> </div></div>';
         $('body').append(html);
         
+         // remove the href attribute of hamburger menu button
+        $('.hambruger.-menu.navbar-toggle').removeAttr('href');
+        
         $('.hambruger.-menu.navbar-toggle').click(function () {
+            var host = $(location).attr('hostname')
+            var path = 'http://'+host+'/menu';            
             $('.modal-body').html('');
             $('.modal-body').html('<img src="http://www.volantski.com/season1516/images/loadingIMG.gif" class="popup-loader">');            
-            $('.modal-body').load("http://jer-cin.tikkewebsites.com/menu #main .region-content");
+            $('.modal-body').load(path+" #main .region-content");
         });
 
     });
