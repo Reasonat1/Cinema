5cb7b47d8946090f3e37e667afb35ce23358c86e
pradeep981 <pradeep.sharma@brihaspatitech.com>
2016-06-28 17:52:30 +0530

    Hide user icon from header

--- sites/all/themes/cinemateque/js/cinemateque.script.js
+++ sites/all/themes/cinemateque/js/cinemateque.script.js
@@ -13,7 +13,7 @@
         var html = '<div id="myModal" class="modal fade hamburger" role="dialog"><div class="modal-dialog"><div class="modal-content"> <img src="/sites/all/themes/cinemateque/images/close42.png" class="close" data-dismiss="modal"/><div class="modal-body"></div></div> </div></div>';
         $('body').append(html);
         
-        
+     
         $('.hambruger.-menu.navbar-toggle').click(function () {
             var host = $(location).attr('hostname')
             var path = 'http://'+host+'/menu';            
@@ -22,12 +22,22 @@
             $('.modal-body').load(path+" #main .region-content");
         });
 
-      
         //$screenheight = $(windows).height()-55;
         //$(".panels-flexible-row-node_page-3 .panels-flexible-region-inside-last").height($screenheight);
+      /**
+        * news letter check box disabled
+        */
+        jQuery('.webform-client-form .webform-component--approve-recieving-e-mail .form-checkbox').attr('disabled',true);
      });
+    /**
+     * Trigger event on ajax complete
+     */
+     jQuery(document).ajaxComplete(function () {
+          jQuery('.webform-client-form .webform-component--approve-recieving-e-mail .form-checkbox').attr('disabled',true);
+    });
 
 })(jQuery);
+
 jQuery(window).scroll(function () {
     if (jQuery(window).scrollTop() > 29 && !(jQuery('#header').hasClass('sticky-header')))
     {
