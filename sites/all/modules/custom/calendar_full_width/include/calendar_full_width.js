ea1768814da23b7fc69b42d033b9a86e8e17c2be
Debasish <debasish.mori@gmail.com>
2016-07-22 11:40:34 +0530

    Add calendar full width module

---
+++ sites/all/modules/custom/calendar_full_width/include/calendar_full_width.js
@@ -0,0 +1,30 @@
+﻿jQuery(document).ready(function () {
+    /**
+     * using Ajax filter the  calender event
+     */
+    jQuery('.calenders .calender-filter > p').click(function () {
+        jQuery(".load-inner").addClass('loading');
+        jQuery('.calenders .calender-filter > p').removeClass('active');
+        jQuery(this).addClass('active');
+        var filterdate = jQuery(this).find('.filter-date.element-invisible').text();
+        url = Drupal.settings.basePath + "ajax_complex_calender_full_width";
+        jQuery.ajax({
+            type: 'post',
+            url: url,
+            dataType: 'json',
+            data: {
+                filterdate: filterdate,
+            },
+            success: function (data) {
+                jQuery('.ajax-inner').replaceWith(data.output);
+                jQuery(".load-inner").removeClass('loading');
+                jQuery(".calendar-floating-top-content span.cal-add-event a").addClass('addthisevent-drop');
+                
+            }
+        });
+    });
+    jQuery(".cal-add-event a").click(function(e) {
+        e.stopPropagation();
+     });
+    
+});
