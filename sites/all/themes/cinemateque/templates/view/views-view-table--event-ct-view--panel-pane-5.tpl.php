45978514a5f502e1a10369f5d82d316a62fb844b
Debasish <debasish.mori@gmail.com>
2016-08-04 13:08:11 +0530

    Hide Empty value

--- sites/all/themes/cinemateque/templates/view/views-view-table--event-ct-view--panel-pane-5.tpl.php
+++ sites/all/themes/cinemateque/templates/view/views-view-table--event-ct-view--panel-pane-5.tpl.php
@@ -34,83 +34,88 @@
    $result = db_query("SELECT DISTINCT node.nid AS nid, field_data_field_cm_event_time.field_cm_event_time_value AS field_data_field_cm_event_time_field_cm_event_time_value FROM {node} node LEFT JOIN {field_data_field_cm_event_lineup} field_data_field_cm_event_lineup ON node.nid = field_data_field_cm_event_lineup.entity_id AND (field_data_field_cm_event_lineup.entity_type = 'node' AND field_data_field_cm_event_lineup.deleted = '0') LEFT JOIN {node} node_field_data_field_cm_event_lineup ON field_data_field_cm_event_lineup.field_cm_event_lineup_target_id = node_field_data_field_cm_event_lineup.nid LEFT JOIN {field_data_field_cm_event_time} field_data_field_cm_event_time ON node.nid = field_data_field_cm_event_time.entity_id AND (field_data_field_cm_event_time.entity_type = 'node' AND field_data_field_cm_event_time.deleted = '0')
 WHERE (( (field_data_field_cm_event_lineup.field_cm_event_lineup_target_id = '$movie_nid' ) )AND(( (node.status = '1') AND (node_field_data_field_cm_event_lineup.type IN  ('$types')) AND (node.language IN  ('$lang_name')) )))
 ORDER BY field_data_field_cm_event_time_field_cm_event_time_value ASC")->fetchAll();
-   $output .= '<div class="table-responsive">';
-    $output .= '<table class="views-table cols-8 table table-striped table-bordered">';
-      $output .= '<thead>';
-        $output .= '<tr>';
-          $output .= '<th class="views-field views-field-field-cm-event-time">'. t('Date') .'</th>';
-          $output .= '<th class="views-field views-field-field-cm-event-time-1">'. t('Time') .'</th>';
-          $output .= '<th class="views-field views-field-field-cm-event-hall">'. t('Hall') .'</th>';
-          $output .= '<th class="views-field views-field-field-cm-event-short-title">'. t('Event') .'</th>';
-          $output .= '<th class="views-field views-field-field-cm-event-internal-id">'. t('Code') .'</th>';
-          $output .= '<th class="views-field views-field-ops"></th>';
-          $output .= '<th class="views-field views-field-php"></th>';
-          $output .= '<th class="views-field views-field-field-toptix-purchase">'. t('Order Tickets') .'</th>';
-        $output .= '</tr>';
-      $output .= '</thead>';
-      $output .= '<tbody>';
-      $a= 0;
-      $row_count = count($result);
-      foreach($result as $val){
-        $a++;
-        $node = node_load($val->nid);
-        
-        if($val->nid !=$current_nid){
-          if(!empty($node->nid)){
-            $event_title = (!empty($node->field_cm_event_short_title)) ? $node->field_cm_event_short_title['und'][0]['value'] : $node->title;
-          }else{
-            $event_title = '<div class="hide-div"></div>';
-          }
-          $path = drupal_get_path_alias('node/'.$node->nid);
-           $flag = flag_create_link('favorite_', $node->nid);
-           $addevent = '<div class="views-field views-field-php">'._return_addthisevent_markup($node).'</div>';
-           if(!empty($node->field_cm_event_internal_id['und'])){
-               $event_code = $node->field_cm_event_internal_id['und'][0]['value'];
-           }else{
-             $event_code = '<div class="hide-div"></div>';
-           }
-            if(!empty($node->field_cm_event_time['und'])){
-              $event_date = date('l d.m.y', $node->field_cm_event_time['und'][0]['value']);
-            }else{
-               $event_date = '<div class="hide-div"></div>';
-            }
-           if(!empty($node->field_cm_event_time['und'])){
-               $event_time = date('g:i a', $node->field_cm_event_time['und'][0]['value']);
-           }else{
-               $event_time = '<div class="hide-div"></div>';
-           }
-           if(!empty($node->field_cm_event_hall['und'])){
-               $hall_id = taxonomy_term_load($node->field_cm_event_hall['und'][0]['target_id']);
-               $hall_name = $hall_id->name;
-           }else{
-             $hall_name = '<div class="hide-div"></div>';
-           }
-           if(!empty($node->field_toptix_purchase['und'])){
-              $toptix_code = $node->field_toptix_purchase['und'][0]['value'];
-              $top_link = 'http://tickets.jer-cin.org.il/loader.aspx/?target=hall.aspx?event='.$toptix_code.'';
-              $puchase = '<button data-url="'.$top_link.'" class="toptix-purchase">'.t('Puchase').'</button>';
-           }else{
-            $puchase = '<div class="hide-div"></div>';
-           }
-         $output .= '<tr class="odd views-row-first views-row-last item-show-'.$a.'">';
-          $output .= '<td class="views-field views-field-field-cm-event-time">'. $event_date. '</td>';
-          $output .= '<td class="views-field views-field-field-cm-event-time-1">'. $event_time .'</td>';
-          $output .= '<td class="views-field views-field-field-cm-event-hall">'. $hall_name .'</td>';
-          $output .= '<td class="views-field views-field-field-cm-event-short-title">'. l($event_title, $path) .'</td>';
-          $output .= '<td class="views-field views-field-field-cm-event-internal-id">' .$event_code .'</td>';
-          $output .= '<td class="views-field views-field-ops">' .$flag .'</td>';
-          $output .= '<td class="views-field views-field-php">' ._return_addthisevent_markup($node) .'</td>';
-          $output .= '<td class="views-field views-field-field-toptix-purchase">'.$puchase.'</td>';
+  $row_count = count($result);
+  if($row_count > 1){
+    $output .= '<div class="table-responsive">';
+     $output .= '<table class="views-table cols-8 table table-striped table-bordered">';
+       $output .= '<thead>';
+         $output .= '<tr>';
+           $output .= '<th class="views-field views-field-field-cm-event-time">'. t('Date') .'</th>';
+           $output .= '<th class="views-field views-field-field-cm-event-time-1">'. t('Time') .'</th>';
+           $output .= '<th class="views-field views-field-field-cm-event-hall">'. t('Hall') .'</th>';
+           $output .= '<th class="views-field views-field-field-cm-event-short-title">'. t('Event') .'</th>';
+           $output .= '<th class="views-field views-field-field-cm-event-internal-id">'. t('Code') .'</th>';
+           $output .= '<th class="views-field views-field-ops"></th>';
+           $output .= '<th class="views-field views-field-php"></th>';
+           $output .= '<th class="views-field views-field-field-toptix-purchase">'. t('Order Tickets') .'</th>';
          $output .= '</tr>';
-        }
-      }
-      $output .= '</tbody>';
-    $output .= '</table>';
-      $output .='<div class="view-footer">';
-      if($row_count > 4){
-       $output .='<div class="more-event">'. t('More Events').'</div>';
-      }
-      $output .='</div>';
-  $output .= '</div>';
-  print $output;
+       $output .= '</thead>';
+       $output .= '<tbody>';
+       $a= 0;
+       $row_count = count($result);
+       foreach($result as $val){
+         $a++;
+         $node = node_load($val->nid);
+         
+         if($val->nid !=$current_nid){
+           if(!empty($node->nid)){
+             $event_title = (!empty($node->field_cm_event_short_title)) ? $node->field_cm_event_short_title['und'][0]['value'] : $node->title;
+           }else{
+             $event_title = '<div class="hide-div"></div>';
+           }
+           $path = drupal_get_path_alias('node/'.$node->nid);
+            $flag = flag_create_link('favorite_', $node->nid);
+            $addevent = '<div class="views-field views-field-php">'._return_addthisevent_markup($node).'</div>';
+            if(!empty($node->field_cm_event_internal_id['und'])){
+                $event_code = $node->field_cm_event_internal_id['und'][0]['value'];
+            }else{
+              $event_code = '<div class="hide-div"></div>';
+            }
+             if(!empty($node->field_cm_event_time['und'])){
+               $event_date = date('l d.m.y', $node->field_cm_event_time['und'][0]['value']);
+             }else{
+                $event_date = '<div class="hide-div"></div>';
+             }
+            if(!empty($node->field_cm_event_time['und'])){
+                $event_time = date('g:i a', $node->field_cm_event_time['und'][0]['value']);
+            }else{
+                $event_time = '<div class="hide-div"></div>';
+            }
+            if(!empty($node->field_cm_event_hall['und'])){
+                $hall_id = taxonomy_term_load($node->field_cm_event_hall['und'][0]['target_id']);
+                $hall_name = $hall_id->name;
+            }else{
+              $hall_name = '<div class="hide-div"></div>';
+            }
+            if(!empty($node->field_toptix_purchase['und'])){
+               $toptix_code = $node->field_toptix_purchase['und'][0]['value'];
+               $top_link = 'http://tickets.jer-cin.org.il/loader.aspx/?target=hall.aspx?event='.$toptix_code.'';
+               $puchase = '<button data-url="'.$top_link.'" class="toptix-purchase">'.t('Puchase').'</button>';
+            }else{
+             $puchase = '<div class="hide-div"></div>';
+            }
+          $output .= '<tr class="odd views-row-first views-row-last item-show-'.$a.'">';
+           $output .= '<td class="views-field views-field-field-cm-event-time">'. $event_date. '</td>';
+           $output .= '<td class="views-field views-field-field-cm-event-time-1">'. $event_time .'</td>';
+           $output .= '<td class="views-field views-field-field-cm-event-hall">'. $hall_name .'</td>';
+           $output .= '<td class="views-field views-field-field-cm-event-short-title">'. l($event_title, $path) .'</td>';
+           $output .= '<td class="views-field views-field-field-cm-event-internal-id">' .$event_code .'</td>';
+           $output .= '<td class="views-field views-field-ops">' .$flag .'</td>';
+           $output .= '<td class="views-field views-field-php">' ._return_addthisevent_markup($node) .'</td>';
+           $output .= '<td class="views-field views-field-field-toptix-purchase">'.$puchase.'</td>';
+          $output .= '</tr>';
+         }
+       }
+       $output .= '</tbody>';
+     $output .= '</table>';
+       $output .='<div class="view-footer">';
+       if($row_count > 4){
+        $output .='<div class="more-event">'. t('More Events').'</div>';
+       }
+       $output .='</div>';
+    $output .= '</div>';
+    print $output;
+  }else{
+    print '<div class="hide-table"></div>';
+  }
 ?>
