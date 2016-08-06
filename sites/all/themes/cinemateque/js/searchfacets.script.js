78c99123b58c4adc0009e46b5f643a56f087c830
Debasish <debasish.mori@gmail.com>
2016-08-06 10:45:36 +0530

    Hide other screening

--- sites/all/themes/cinemateque/js/searchfacets.script.js
+++ sites/all/themes/cinemateque/js/searchfacets.script.js
@@ -9,4 +9,8 @@ jQuery(document).ready(function () {
     jQuery(".popup_search").hide();
     jQuery(".search_new").appendTo("#search-api-page-search-form");
     jQuery("#search-api-page-search-form").appendTo(".container:first");
+           /******Hide Other screening ****/
+    if(jQuery('.pane-event-ct-view-panel-pane-5 .view-content .hide-table').length){
+        jQuery('.pane-event-ct-view-panel-pane-5 h2.pane-title').hide();
+    }
 });
