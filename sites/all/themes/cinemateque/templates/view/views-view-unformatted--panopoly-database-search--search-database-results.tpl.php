d214ba07d7ca50e932dcc73dfe85dc9b4fe77e1d
Debasish <debasish.mori@gmail.com>
2016-09-27 14:43:32 +0530

    Strange loop behaviour that display screening - Bug Fixed

--- sites/all/themes/cinemateque/templates/view/views-view-unformatted--panopoly-database-search--search-database-results.tpl.php
+++ sites/all/themes/cinemateque/templates/view/views-view-unformatted--panopoly-database-search--search-database-results.tpl.php
@@ -22,6 +22,11 @@
 <?php
   $results=$view->result;
   foreach ($results as $val) {
+    global $language ;
+    $lang_name = isset($language->language) ? $language->language : '';
+    if($lang_name == ''){
+      $lang_name = 'en';
+    }
     $node = node_load($val->entity);
     $default_image = '<img src="/sites/all/themes/cinemateque/images/default-image-pane-2.png">';
     $default_user_image = '<img src="/sites/all/themes/cinemateque/images/user-default.jpg">';
@@ -262,88 +267,129 @@
            $output_event .= '</tr>';
          $output_event .= '</table>';
        $output_event .= '</div>';
-    }else{
-     $output ='';
-     $output .='<div class="table-responsive">';
-        $output .= '<table class="table">';
-         $output .= ' <tbody>';
-         /********For Movie****/
-        if(!empty($node->field_event_corresponding_ref['und'])){
-            $movie_node_info = node_load($node->field_event_corresponding_ref['und'][0]['target_id']);
-            $line_event = $movie_node_info->field_cm_event_lineup['und'];       
+    }
+    if($node->type == 'cm_movie_group'){
+    $result = db_query("SELECT DISTINCT node.nid AS nid, field_data_field_cm_event_time.field_cm_event_time_value AS field_data_field_cm_event_time_field_cm_event_time_value FROM {node} node LEFT JOIN {field_data_field_cm_event_lineup} field_data_field_cm_event_lineup ON node.nid = field_data_field_cm_event_lineup.entity_id AND (field_data_field_cm_event_lineup.entity_type = 'node' AND field_data_field_cm_event_lineup.deleted = '0') LEFT JOIN {node} node_field_data_field_cm_event_lineup ON field_data_field_cm_event_lineup.field_cm_event_lineup_target_id = node_field_data_field_cm_event_lineup.nid LEFT JOIN {field_data_field_cm_event_time} field_data_field_cm_event_time ON node.nid = field_data_field_cm_event_time.entity_id AND (field_data_field_cm_event_time.entity_type = 'node' AND field_data_field_cm_event_time.deleted = '0') WHERE (( (field_data_field_cm_event_lineup.field_cm_event_lineup_target_id = '$node->nid') )AND(( (node.status = '1') AND (node_field_data_field_cm_event_lineup.type IN  ('cm_movie_group'))))) ORDER BY field_data_field_cm_event_time_field_cm_event_time_value ASC")->fetchAll();
+    $row_count = count($result);
+    $output ='';
+    $output .='<div class="table-responsive">';
+      $output .= '<table class="table">';
+        $output .= ' <tbody>';
+        foreach($result as $val){
+          $movie_group_event_nid = $val->nid;
+          $movie_group_event_info = node_load($movie_group_event_nid);
+          $flag_moviegroup_event = flag_create_link('favorite_', $movie_group_event_info->nid);
+          $event_title = t($movie_group_event_info->title);
+          $path = drupal_get_path_alias('node/'.$movie_group_event_info->nid);
+          $addevent = '<div class="views-field views-field-php">'._return_addthisevent_markup($movie_group_event_info).'</div>';
+          if(!empty($movie_group_event_info->field_cm_event_time['und'])){
+           $event_date = date('l d.m.y', $movie_group_event_info->field_cm_event_time['und'][0]['value']);
+           $event_date_mobile = date('d.m.y', $movie_group_event_info->field_cm_event_time['und'][0]['value']);
+           $event_time = date('G:i', $movie_group_event_info->field_cm_event_time['und'][0]['value']);
+          }
+          $output .= '<tr class="row-custom-lobby">';
+           $output .= '<td class="date only-desktop">'.t($event_date).'</td>';
+           $output .= '<td class="time"><div class="only-mobile">'.$event_date_mobile.'</div>'.$event_time.'</td>';
+           if(!empty($movie_group_event_info->field_cm_event_hall['und'])){
+               $hall_id = taxonomy_term_load($movie_group_event_info->field_cm_event_hall['und'][0]['target_id']);
+               $hall_name = $hall_id->name;
+               $output .= '<td class="hall only-desktop">'.t($hall_name).'</td>';
+           }
+           else{
+               $hall_name = "";
+               $output .= '<td class="hall only-desktop"></td>';
+           }
+           $output .= '<td class="title only-desktop">';
+           if(!empty($event_title)){
+             $output .= l($event_title, $path);
+           }
+           $output .= '</td>';
+           if(!empty($movie_group_event_info->field_cm_event_internal_id['und'])){
+             $event_code = $movie_group_event_info->field_cm_event_internal_id['und'][0]['value'];
+             $output .= '<td class="code"><div class="only-mobile">'.$hall_name.'</div>'.t($event_code).'</td>';
+           }
+           else{
+             $output .= '<td class="code"><div class="only-mobile">'.$hall_name.'</div></td>';
+           }
+           $output .='<td class="flag only-desktop">'. $flag_moviegroup_event . '</td>';
+           $output .='<td class="add-event only-desktop">'. $addevent . '</td>';
+           if(!empty($movie_group_event_info->field_toptix_purchase['und'])){
+           $toptix_code = $movie_group_event_info->field_toptix_purchase['und'][0]['value'];
+           $top_link = 'http://199.203.164.53/loader.aspx/?target=hall.aspx?event='.$toptix_code.'';
+           $output .= '<td class="purchase">'.'<button data-url="'.$top_link.'" class="toptix-purchase">'.t("TICKETS").'</button>'.'</td>';
+           } 
+           else{
+             $output .= '<td class="purchase"></td>';
+           }
+          $output .= '</tr>';
         }
-        /****Movie Group Nid****/ 
-        if(!empty($node->field_movie_referenced['und'])){
-            $movie_node = node_load($node->field_movie_referenced['und'][0]['target_id']);
+        $output .= ' </tbody>';
+      $output .= '</table>';
+    $output .= '</div>';  
+    }
+    if($node->type == 'cm_movie'){
+    $results = db_query("SELECT DISTINCT node.nid AS nid, field_data_field_cm_event_time.field_cm_event_time_value AS field_data_field_cm_event_time_field_cm_event_time_value
+    FROM {node} node LEFT JOIN {field_data_field_cm_event_lineup} field_data_field_cm_event_lineup ON node.nid = field_data_field_cm_event_lineup.entity_id AND (field_data_field_cm_event_lineup.entity_type = 'node' AND field_data_field_cm_event_lineup.deleted = '0') LEFT JOIN {node} node_field_data_field_cm_event_lineup ON field_data_field_cm_event_lineup.field_cm_event_lineup_target_id = node_field_data_field_cm_event_lineup.nid LEFT JOIN {field_data_field_cm_event_time} field_data_field_cm_event_time ON node.nid = field_data_field_cm_event_time.entity_id AND (field_data_field_cm_event_time.entity_type = 'node' AND field_data_field_cm_event_time.deleted = '0')
+    WHERE (( (field_data_field_cm_event_lineup.field_cm_event_lineup_target_id = '$node->nid' ) )AND(( (node.status = '1') AND (node_field_data_field_cm_event_lineup.type IN  ('cm_movie')) AND (node.language IN  ('$lang_name')))))
+    ORDER BY field_data_field_cm_event_time_field_cm_event_time_value ASC")->fetchAll();
+    $row_count = count($results);
+    $output_movie_event ='';
+    $output_movie_event .='<div class="table-responsive">';
+      $output_movie_event .= '<table class="table">';
+        $output_movie_event .= ' <tbody>';
+        foreach($results as $val){
+          $movie_event_nid = $val->nid;
+          $movie_event_info = node_load($movie_event_nid);
+          $flag_moviegroup_event = flag_create_link('favorite_', $movie_event_info->nid);
+          $event_title = t($movie_event_info->title);
+         $path = drupal_get_path_alias('node/'.$movie_event_info->nid);
+         $addevent = '<div class="views-field views-field-php">'._return_addthisevent_markup($movie_event_info).'</div>';
+         if(!empty($movie_event_info->field_cm_event_time['und'])){
+          $event_date = date('l d.m.y', $movie_event_info->field_cm_event_time['und'][0]['value']);
+          $event_date_mobile = date('d.m.y', $movie_event_info->field_cm_event_time['und'][0]['value']);
+          $event_time = date('G:i', $movie_event_info->field_cm_event_time['und'][0]['value']);
+         }
+          $output_movie_event .= '<tr class="row-custom-lobby">';
+           $output_movie_event .= '<td class="date only-desktop">'.t($event_date).'</td>';
+           $output_movie_event .= '<td class="time"><div class="only-mobile">'.$event_date_mobile.'</div>'.$event_time.'</td>';
+           if(!empty($movie_event_info->field_cm_event_hall['und'])){
+               $hall_id = taxonomy_term_load($movie_event_info->field_cm_event_hall['und'][0]['target_id']);
+               $hall_name = $hall_id->name;
+               $output_movie_event .= '<td class="hall only-desktop">'.t($hall_name).'</td>';
+           }
+           else{
+               $hall_name = "";
+               $output_movie_event .= '<td class="hall only-desktop"></td>';
+           }
+           $output_movie_event .= '<td class="title only-desktop">';
+           if(!empty($event_title)){
+             $output_movie_event .= l($event_title, $path);
+           }
+           $output_movie_event .= '</td>';
+           if(!empty($movie_event_info->field_cm_event_internal_id['und'])){
+             $event_code = $movie_event_info->field_cm_event_internal_id['und'][0]['value'];
+             $output_movie_event .= '<td class="code"><div class="only-mobile">'.$hall_name.'</div>'.t($event_code).'</td>';
+           }
+           else{
+             $output_movie_event .= '<td class="code"><div class="only-mobile">'.$hall_name.'</div></td>';
+           }
+           $output_movie_event .='<td class="flag only-desktop">'. $flag_moviegroup_event . '</td>';
+           $output_movie_event .='<td class="add-event only-desktop">'. $addevent . '</td>';
+           if(!empty($movie_event_info->field_toptix_purchase['und'])){
+           $toptix_code = $movie_event_info->field_toptix_purchase['und'][0]['value'];
+           $top_link = 'http://199.203.164.53/loader.aspx/?target=hall.aspx?event='.$toptix_code.'';
+           $output_movie_event .= '<td class="purchase">'.'<button data-url="'.$top_link.'" class="toptix-purchase">'.t("TICKETS").'</button>'.'</td>';
+           } 
+           else{
+             $output_movie_event .= '<td class="purchase"></td>';
+           }
+          $output_movie_event .= '</tr>';
         }
-        if(!empty($movie_node->field_event_corresponding_ref['und'])){
-          $event_node = node_load($movie_node->field_event_corresponding_ref['und'][0]['target_id']);
-          //drupal_set_message('<pre>'.print_r($event_node->nid, 1).'</pre>');
-           $line_event = $event_node->field_cm_event_lineup['und'];
-        }
-        /****Movie Group Nid****/
-        if(!empty($line_event)){
-            foreach ($line_event as $row) {
-              $event_ref_nid = $row['target_id'];
-              if(!empty($row['target_id'])){
-                $node_movie = node_load($event_ref_nid);
-              } 
-              if(!empty($node_movie->field_event_corresponding_ref['und'])){
-                $node_event = node_load($node_movie->field_event_corresponding_ref['und'][0]['target_id']);
-                if(!empty($node_event->field_cm_event_short_title['und'])){
-                  $event_title = t($node_event->field_cm_event_short_title['und'][0]['value']);
-                }
-                $path = drupal_get_path_alias('node/'.$node_event->nid);
-                $flags = flag_create_link('favorite_', $node_event->nid);
-              }
-              $addevent = '<div class="views-field views-field-php">'._return_addthisevent_markup($node_event).'</div>';
-              if(!empty($node_event->field_cm_event_time['und'])){
-                  $event_date = date('l d.m.y', $node_event->field_cm_event_time['und'][0]['value']);
-                  $event_date_mobile = date('d.m.y', $node_event->field_cm_event_time['und'][0]['value']);
-                  $event_time = date('G:i', $node_event->field_cm_event_time['und'][0]['value']);
-              }
+        $output_movie_event .= ' </tbody>';
+      $output_movie_event .= '</table>';
+    $output_movie_event .= '</div>';  
+    }
 
-              $output .= '<tr class="row-custom-lobby">';
-                $output .= '<td class="date only-desktop">'.t($event_date).'</td>';
-                $output .= '<td class="time"><div class="only-mobile">'.$event_date_mobile.'</div>'.$event_time.'</td>';
-                if(!empty($node_event->field_cm_event_hall['und'])){
-                  $hall_id = taxonomy_term_load($node_event->field_cm_event_hall['und'][0]['target_id']);
-                  $hall_name = $hall_id->name;
-                  $output .= '<td class="hall only-desktop">'.$hall_name.'</td>';
-                }
-                else{
-                  $hall_name = "";
-                  $output .= '<td class="hall only-desktop"></td>';
-                }
-                if(!empty($event_title)){
-                  $output .= '<td class="title only-desktop">'.l($event_title, $path).'</td>';
-                }
-                else{
-                  $output .= '<td class="title only-desktop"></td>';
-                }
-                if(!empty($node_event->field_cm_event_internal_id['und'])){
-                  $event_code = $node_event->field_cm_event_internal_id['und'][0]['value'];
-                  $output .= '<td class="code"><div class="only-mobile">'.$hall_name.'</div>'.t($event_code).'</td>';
-                }
-                else{
-                  $output .= '<td class="code"><div class="only-mobile">'.$hall_name.'</div></td>';
-                }
-                $output .='<td class="flag only-desktop">'. $flags . '</td>';
-                $output .='<td class="add-event only-desktop">'. $addevent . '</td>';
-                if(!empty($node_event->field_toptix_purchase['und'])){
-                  $toptix_code = $node_event->field_toptix_purchase['und'][0]['value'];
-                  $top_link = 'http://199.203.164.53/loader.aspx/?target=hall.aspx?event='.$toptix_code.'';
-                  $output .= '<td class="purchase">'.'<button data-url="'.$top_link.'" class="toptix-purchase">' . t("TICKETS") . '</button>'.'</td>';
-                }
-                else{
-                  $output .= '<td class="purchase"></td>';
-                }
-                $output .= '</tr>';
-            }
-        }
-         $output .= '</table>';
-       $output .= '</div>';
-    }  
       switch ($node->type) {
         case "cm_person":
           $image = '<div class="person-left-sec"><div class="person-img">'.l($image_person, "$path_node", array('attributes' => array('class' =>'link-image'),'html' => true)) .'</div><div class="lobby-title">'. l($first_name .' '. $last_name, $path_node).'</div><div class="job-title">'. $job_title.'</div></div>';
@@ -365,7 +411,7 @@
           $image = '<div class="image-container-gorup"><div class="flag-new-links">'.$flag.'</div><div class="image-container">'.l($image_movie, "$path_node", array('attributes' => array('class' =>'link-image'),'html' => true)).'</div></div>';
           $title = $title;
           $sort_summary = t($summary_movie);
-          $event_info = $output;
+          $event_info = $output_movie_event;
           $duration_info = $movie_credit . $length;
           $top_text = $black_text_movie . $white_text_movie;
         break;
