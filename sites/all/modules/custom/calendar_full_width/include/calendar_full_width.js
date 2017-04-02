2c9e7a26c16ce4d2a64939bb0a4abd3833f639dd
alex <alex@reasonat.com>
2017-04-02 18:32:33 +0300

    Add calendar in full calendar

--- sites/all/modules/custom/calendar_full_width/include/calendar_full_width.js
+++ sites/all/modules/custom/calendar_full_width/include/calendar_full_width.js
@@ -22,6 +22,7 @@
             success: function (data) {
                 var path_url = Drupal.settings.basePath + 'sites/all/modules/flag/theme/flag.js';
                 var cal_path = Drupal.settings.basePath + 'sites/all/modules/custom/my_utilities/include/atc.min.js';
+                var cal_path_css = Drupal.settings.basePath + 'sites/all/modules/custom/my_utilities/include/atc-style-button-icon.css';
                 var cal_patha = Drupal.settings.basePath + 'sites/all/modules/custom/my_utilities/include/addcalendar.js';
                 var cal_path_ticket = Drupal.settings.basePath + 'sites/all/modules/cinematic_custom/cinematic_toptix/frame.js';
                 jQuery('.ajax-inner').replaceWith(data.output);
@@ -29,7 +30,7 @@
                 jQuery.getScript(path_url, function() {
                     Drupal.behaviors.flagLink.attach(document);
                 });
-                jQuery.getScript(cal_path);
+                jQuery.getScript(cal_path, function () {jQuery('head').append('<link rel="stylesheet" href="'+cal_path_css+'" type="text/css" />');});
                 jQuery.getScript(cal_patha);
                 jQuery.getScript(cal_path_ticket, function(){Drupal.behaviors.toptix_frame.attach(document);});
             }
