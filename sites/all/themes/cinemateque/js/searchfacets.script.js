ad01180ff965541056377c3c04b33529af46b68f
alex <alex@reasonat.com>
2017-04-12 10:14:42 +0300

    Search JS

--- sites/all/themes/cinemateque/js/searchfacets.script.js
+++ sites/all/themes/cinemateque/js/searchfacets.script.js
@@ -5,7 +5,7 @@
 
 jQuery(document).ready(function () {
     var popup = jQuery(".popup_search").html();
-    jQuery(".popup-element-title span").html(popup);
+    //jQuery(".popup-element-title span").html(popup);
     jQuery(".popup_search").hide();
     jQuery(".search_new").appendTo("#search-api-page-search-form");
     jQuery("#search-api-page-search-form").appendTo(".container:first");
