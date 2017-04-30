165e293d479fba9a5d4da07e5a93f2543df989d7
jercin <jercin@reasonat>
2017-04-30 13:27:48 +0300

    esro example update

---
+++ esro/nav.js
@@ -0,0 +1,64 @@
+var toptix_setup = {};
+
+$(document).ready(function() {
+  toptix_setup.basket();
+  toptix_setup.purchase();
+  $('button.hide').click(function() {
+    var frame = $('#toptix-frame-wrapper');
+    frame.hide();
+  });
+});
+
+if ($.cookie('toptix_basket') == null) {
+  $.cookie('toptix_basket', 0);
+}
+
+toptix_setup.basket = function () {
+  $('button.basket').click(function(event) {
+    if ($.cookie('toptix_basket') == 0) {
+      alert('You have not added any items to purchase yet');
+      return;
+    }
+    toptix_open_basket_frame(this.dataset.url);
+  });
+};
+
+function toptix_open_basket_frame(basket_url) {
+  // wrapper div for <esro:frame
+  var frame = $('#toptix-frame-wrapper');
+  $esro.gotoUrl(basket_url);
+  frame.show();
+}
+
+var toptix_active_button = {original_text:'', item: null};
+var toptix_purchase_setup = null;
+toptix_setup.purchase = function() {
+  $('button.purchase').click(function(event) {
+    toptix_active_button.item = this;
+    toptix_active_button.original_text = $(this).text();
+    $(this).text('Loading...');
+    toptix_open_frame(this.dataset.url);
+  });
+};
+
+function toptix_open_frame(event_url) {
+  var frame = jQuery('#toptix-frame-wrapper');
+  $esro.gotoUrl(event_url);
+  frame.show();
+  jQuery(toptix_active_button.item).text(toptix_active_button.original_text);
+}
+
+$esro.attachEventHandler('basketChanged', function(basket) {
+  console.log('my basket handler');
+  console.log(basket);
+  jQuery.cookie('toptix_basket', basket.Tickets.length);
+});
+
+$esro.attachEventHandler('navigationRequired', function (pageName) {
+  console.log('esro navigation:' + pageName);
+  if (pageName == 'DEFAULT') {
+    var target_page = '/esro/nav.html';
+    var target_address = 'http://jer-cin.reasonat.com' + target_page;
+    window.location.href = target_address;
+  }
+});
